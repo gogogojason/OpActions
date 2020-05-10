812fe71612b20e42b4fc0b5c4ee38269548af242
hfy166 <54941004+hfy166@users.noreply.github.com>
2020-05-10 17:28:36 +0800

    Update public.sh

--- openwrt/public/public.sh
+++ openwrt/public/public.sh
@@ -1,23 +1,25 @@
 # cd $WORK_PATH 目录下,先运行的 public.h -> 设备.h -> scripts/feeds install -a
-# 必须的文件
-device_name='MiRouter'      # 设备名，放到了单独设备的sh文件中了
-wifi_name="RMWiFi"       # Wifi 名字 ,放到了单独设备的sh文件中了
-lan_ip='192.168.2.1'        # Lan Ip地址
-utc_name='Asia\/Shanghai'   # 时区
-delete_bootstrap=false       # 是否删除默认主题 true 、false
-default_theme='luci-theme-argon-light-mod'   # 默认主题 结合主题文件夹名字
-theme_argon='https://github.com/sypopo/luci-theme-argon-mc.git'  # 主题地址
-openClash_url='https://github.com/vernesong/OpenClash.git'       # OpenClash包地址
+# 定义部分以及需要添加对应APP必须的文件
+device_name='MiRouter'                                                      # 自定义设备名
+wifi_name="RMWiFi"                                                          # 自定义Wifi 名字
+lan_ip='192.168.2.1'                                                        # 自定义Lan Ip地址
+utc_name='Asia\/Shanghai'                                                   # 自定义时区
+delete_bootstrap=false                                                      # 是否删除默认主题 true 、false
+default_theme='luci-theme-argon-light-mod'                                  # 默认主题 结合主题文件夹名字
+theme_argon='https://github.com/sypopo/luci-theme-argon-mc.git'             # 主题地址
+openClash_url='https://github.com/vernesong/OpenClash.git'                  # OpenClash包地址
 adguardhome_url='https://github.com/rufengsuixing/luci-app-adguardhome.git' # adguardhome 包地址
-lienol_url='https://github.com/Lienol/openwrt-package.git'       # Lienol 包地址
-vssr_url_rely='https://github.com/jerrykuku/lua-maxminddb.git'   # vssr lua-maxminddb依赖
-vssr_url='https://github.com/jerrykuku/luci-app-vssr.git'        # vssr地址
-vssr_plus_rely='https://github.com/Leo-Jo-My/my.git'             # vssr_plus 依赖
-vssr_plus='https://github.com/Leo-Jo-My/luci-app-vssr-plus.git'  # vssr_plus 地址
-filter_url='https://github.com/destan19/OpenAppFilter.git'       # AppFilter 地址
-smartdns_url='https://github.com/pymumu/openwrt-smartdns'
-smartdnsapp_url='https://github.com/pymumu/luci-app-smartdns.git'
-passwall_url='https://github.com/hfy166/Lienol-openwrt-packages-backup'
+lienol_url='https://github.com/Lienol/openwrt-package.git'                  # Lienol 包地址
+vssr_url_rely='https://github.com/jerrykuku/lua-maxminddb.git'              # vssr lua-maxminddb依赖
+vssr_url='https://github.com/jerrykuku/luci-app-vssr.git'                   # vssr地址
+vssr_plus_rely='https://github.com/Leo-Jo-My/my.git'                        # vssr_plus 依赖
+vssr_plus='https://github.com/Leo-Jo-My/luci-app-vssr-plus.git'             # vssr_plus 地址
+filter_url='https://github.com/destan19/OpenAppFilter.git'                  # AppFilter 地址
+smartdns_url='https://github.com/pymumu/openwrt-smartdns'                   # SmartDNS运行程序
+smartdnsapp_url='https://github.com/pymumu/luci-app-smartdns.git'           # SmartDNS-App
+passwall_url='https://github.com/hfy166/Lienol-openwrt-packages-backup'     # Passwall备份包
+
+#下面是执行具体操作
 
 echo "修改机器名称"
 sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
@@ -31,14 +33,8 @@ sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 echo "修改时区"
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
 
-#echo '添加主题argon-light和含主题passwall备份包'
-#git clone $passwall_url package/laohu
-#echo 'CONFIG_PACKAGE_luci-theme-argon-light-mod=y' >> .config
-
 echo "修改默认主题"
 sed -i 's/+luci-theme-bootstrap/+luci-theme-argon-light-mod/g' feeds/luci/collections/luci/Makefile
-
-
 
 echo '添加主题argon'
 git clone $theme_argon package/lean/luci-theme-argon-mc
@@ -57,9 +53,6 @@ git clone $openClash_url package/lean/luci-app-openclash
 echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
 echo 'CONFIG_PACKAGE_luci-i18n-openclash-zh-cn=y'  >> .config
 
-#echo '添加Lienol包'
-#git clone $lienol_url package/Lienol
-
 echo '添加Passwall'
 git clone $passwall_url package/laohu
 echo 'CONFIG_PACKAGE_luci-app-passwall=y' >> .config
@@ -71,20 +64,23 @@ echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Brook=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-passwall_INCLUDE_kcptun=y' >> .config
 echo 'CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y'  >> .config
 
-#echo '添加filebrowser'
-#echo 'CONFIG_PACKAGE_luci-app-filebrowser=y' >> .config
-#echo 'CONFIG_PACKAGE_luci-i18n-filebrowser-zh-cn=y'  >> .config
-
 echo '添加adguardhome'
 git clone $adguardhome_url package/lean/luci-app-adguardhome
 echo 'CONFIG_PACKAGE_luci-app-adguardhome=y' >> .config
 echo 'CONFIG_PACKAGE_luci-i18n-adguardhome-zh-cn=y'  >> .config
+
+#echo '添加filebrowser'
+#echo 'CONFIG_PACKAGE_luci-app-filebrowser=y' >> .config
+#echo 'CONFIG_PACKAGE_luci-i18n-filebrowser-zh-cn=y'  >> .config
 
 # echo '添加HelloWord,并使用包默认的配置'  # TODO 这个的配置文件和SSP 冲突
 # git clone $vssr_url_rely package/lean/lua-maxminddb
 # git clone $vssr_url package/lean/luci-app-vssr
 # echo 'CONFIG_PACKAGE_luci-app-vssr=y' >> .config
 # echo 'CONFIG_PACKAGE_luci-i18n-vssr-zh-cn=y'  >> .config
+
+#echo '添加Lienol包'
+#git clone $lienol_url package/Lienol
 
 #echo '添加OpenAppFilter过滤器'
 #git clone $filter_url package/OpenAppFilter
